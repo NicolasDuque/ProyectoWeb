c7370d1dac0cd76dbb9466847cb71aa6922a17bd
Carlos Andres Torres <caletorres_59@hotmail.com>
2016-09-21 20:51:10 -0500

    Estructura Proyecto
    se crea la estructura del proyecto
    
    tipo: mejora implementacion

--- index.php
+++ index.php
@@ -7,9 +7,14 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <script src="Recursos/jquery/jquery-1.12.0.min.js" type="text/javascript"></script>
+        <link href="Recursos/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <script src="Recursos/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <title>Proyecto Servicios</title>
     </head>
     <body>
+        
+        <h2 class="bg-success">Prueba</h2>
         <?php
         // put your code here
         ?>
